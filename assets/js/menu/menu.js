6e1a6c968171f6282e7c74943bfb395cb902e807
Patrick <ptemy5@gmail.com>
2023-09-22 00:40:44 -0300

    organização

---
+++ assets/js/menu/menu.js
@@ -0,0 +1,9 @@
+import dark from '../dark.js'
+import acc from './acc.js'
+import menuMobile from './menu-mobile.js'
+import dropdown from './dropdown-menu.js'
+
+acc()
+dark()
+menuMobile()
+dropdown()
