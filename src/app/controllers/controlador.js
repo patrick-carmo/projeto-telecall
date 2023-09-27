9665a6c7ef1543cfc0c9badd3f9d5d4864e80787
Patrick <ptemy5@gmail.com>
2023-09-27 20:27:53 -0300

    páginas movidas para a raiz principal
    O GitHub Pages não funciona corretamente se o html estiver fora da raiz principal

--- src/app/controllers/controlador.js
+++ src/app/controllers/controlador.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const controle = {
   paginaPrincipal: (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '..', 'app', 'views', 'principal.html'))
+    res.sendFile(path.join(__dirname, 'principal.html'))
   },
 }
 module.exports = controle
