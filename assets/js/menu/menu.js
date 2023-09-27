5ae47d2e743a4935ba9095c17127a748f64db4d1
Patrick <ptemy5@gmail.com>
2023-09-27 20:27:53 -0300

    Revert "Merge branch 'main' into add"
    This reverts commit f2473eb7121de9552cca34dc2fb1c14e8fe27ba7, reversing
    changes made to 68b328a7719d6e90e9f05e3fe930f2068c74051f.

--- assets/js/menu/menu.js
+++
@@ -1,9 +0,0 @@
-import dark from '../dark.js'
-import acc from './acc.js'
-import menuMobile from './menu-mobile.js'
-import dropdown from './dropdown-menu.js'
-
-acc()
-dark()
-menuMobile()
-dropdown()
