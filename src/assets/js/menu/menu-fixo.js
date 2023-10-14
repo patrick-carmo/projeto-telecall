5d27025511537443b6755f34ae92052a31468969
Patrick <ptemy5@gmail.com>
2023-10-14 17:18:03 -0300

    add - layout final index

--- src/assets/js/menu/menu-fixo.js
+++ src/assets/js/menu/menu-fixo.js
@@ -2,18 +2,25 @@ export default function menuFixo() {
   window.addEventListener('scroll', () => {
     const header = document.querySelector('.header')
     const back = document.querySelector('#back-top')
-    if (window.scrollY > 100){
-      header.classList.add('menu-fixo')
-      back.style.display = 'block'
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth
+    const menuList = document.querySelector("[data-menu='list']")
 
-      back.addEventListener('click', () => {
-        window.scrollTo(0, 0)
-      })
-
-      return
+    if (window.scrollY >= 0 && window.scrollY <= 100) {
+      menuList.style.paddingTop = '90px'
+      header.classList.remove('menu-fixo')
     }
+    if (window.scrollY > 100) {
+      header.classList.add('menu-fixo')
+      menuList.style.paddingTop = ''
 
-    header.classList.remove('menu-fixo')
+      if (screenWidth >= 600) {
+        back.style.display = 'block'
+        back.addEventListener('click', () => {
+          window.scrollTo(0, 0)
+        })
+        return
+      }
+    }
     back.style.display = 'none'
   })
 }
