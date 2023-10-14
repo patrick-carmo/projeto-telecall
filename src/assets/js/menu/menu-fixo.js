3550b2998a38b7229e2d39b29807af98308d5e28
Patrick <ptemy5@gmail.com>
2023-10-14 17:56:27 -0300

    fix - padding menu

--- src/assets/js/menu/menu-fixo.js
+++ src/assets/js/menu/menu-fixo.js
@@ -1,26 +1,46 @@
 export default function menuFixo() {
+  const menuList = document.querySelector("[data-menu='list']")
+  const back = document.querySelector('#back-top')
+
   window.addEventListener('scroll', () => {
     const header = document.querySelector('.header')
-    const back = document.querySelector('#back-top')
     const screenWidth = window.innerWidth || document.documentElement.clientWidth
-    const menuList = document.querySelector("[data-menu='list']")
 
-    if (window.scrollY >= 0 && window.scrollY <= 100) {
-      menuList.style.paddingTop = '90px'
+    if (window.scrollY <= 100) {
+      menuList.style.paddingTop = ''
       header.classList.remove('menu-fixo')
     }
     if (window.scrollY > 100) {
+      if (screenWidth <= 1000) {
+        menuList.style.paddingTop = '60px'
+      }
       header.classList.add('menu-fixo')
-      menuList.style.paddingTop = ''
-
       if (screenWidth >= 600) {
         back.style.display = 'block'
         back.addEventListener('click', () => {
           window.scrollTo(0, 0)
         })
+
         return
       }
     }
     back.style.display = 'none'
   })
+
+  function atualizarPaddingTop() {
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth
+    if (screenWidth > 1000 || window.screenY <= 100) {
+      menuList.style.paddingTop = ''
+    }
+    if (screenWidth <= 1000 && window.scrollY > 100) {
+      menuList.style.paddingTop = '60px'
+    }
+
+    if(screenWidth < 600) {
+      back.style.display = 'none'
+    }
+  }
+
+  atualizarPaddingTop()
+  window.addEventListener('resize', atualizarPaddingTop)
 }
