afc2ab16d8793f29a4770ffef22c1883633544a9
Patrick <ptemy5@gmail.com>
2023-10-14 17:26:02 -0300

    Revert "add - layout final index"
    This reverts commit 5d27025511537443b6755f34ae92052a31468969.

--- src/assets/js/menu/menu-fixo.js
+++ src/assets/js/menu/menu-fixo.js
@@ -2,25 +2,18 @@ export default function menuFixo() {
   window.addEventListener('scroll', () => {
     const header = document.querySelector('.header')
     const back = document.querySelector('#back-top')
-    const screenWidth = window.innerWidth || document.documentElement.clientWidth
-    const menuList = document.querySelector("[data-menu='list']")
-
-    if (window.scrollY >= 0 && window.scrollY <= 100) {
-      menuList.style.paddingTop = '90px'
-      header.classList.remove('menu-fixo')
-    }
-    if (window.scrollY > 100) {
+    if (window.scrollY > 100){
       header.classList.add('menu-fixo')
-      menuList.style.paddingTop = ''
+      back.style.display = 'block'
 
-      if (screenWidth >= 600) {
-        back.style.display = 'block'
-        back.addEventListener('click', () => {
-          window.scrollTo(0, 0)
-        })
-        return
-      }
+      back.addEventListener('click', () => {
+        window.scrollTo(0, 0)
+      })
+
+      return
     }
+
+    header.classList.remove('menu-fixo')
     back.style.display = 'none'
   })
 }
